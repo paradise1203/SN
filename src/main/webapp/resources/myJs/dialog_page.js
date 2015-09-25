30a041d267df96930b3a8b6e3790c18ad77dbafc
Shayfutdinov Aidar <exoooy@gmail.com>
2015-09-26 01:31:53 +0300

    Fixing some merge issues - working fine finally

--- src/main/webapp/resources/myJs/dialog_page.js
+++ src/main/webapp/resources/myJs/dialog_page.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+    var interval = setInterval(function () {
+        var sender = $("#sender").val();
+        var recipient = $("#recipient").val();
+        $.ajax({
+            url: 'getMessage',
+            type: 'GET',
+            data: {
+                sender: sender,
+                recipient: recipient
+            },
+            dataType: 'text',
+            success: function (response) {
+                $("#dialog").append(response);
+            }
+        });
+    }, 3000);
+
     $(".sendMes").click(function () {
         var sender = $("#sender").val();
         var recipient = $("#recipient").val();
